7151ebdc52389350a99cdec6099ed1fb5b231cad
joao-marcos-de-jesus <933323mk@gmail.com>
2025-07-20 17:21:12 -0300

    criação dos pacotes - request.converter e math - implementação das classes - SimpleMath e NumberConverter

--- src/main/java/github/com/_mk/controllers/MathController.java
+++ src/main/java/github/com/_mk/controllers/MathController.java
@@ -1,6 +1,8 @@
 package github.com._mk.controllers;
 
 import github.com._mk.exception.UnsupportedMathOperationException;
+import github.com._mk.math.SimpleMath;
+import github.com._mk.request.converters.NumberConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,34 +10,82 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/math")
 public class MathController {
+
+    private SimpleMath math = new SimpleMath();
+
     // http://localhost:8080/math/sum/3/5
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public double sum(
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
-    )throws Exception{
+    ) {
 
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", "."); // R$ 5,00 -> USD 5.00
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
-
-    private double convertToDouble(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
-        String number = strNumber.replace(",", "."); // R$ 5,00 -> USD 5.00
-        return Double.parseDouble(number);
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     // http://localhost:8080/math/subtraction/3/5
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public double subtraction(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
+        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
 
     // http://localhost:8080/math/division/3/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public double division(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
 
-    // http://localhost:8080/math/sum/3/5
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
+        if ((NumberConverter.convertToDouble(numberOne) == 0 || NumberConverter.convertToDouble(numberTwo) == 0))
+            throw new UnsupportedMathOperationException("IMPOSSIBLE TO DIVIDE BY 0!");
+
+        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
+
+
+    // http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public double multiplication(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
+        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
+
+    // http://localhost:8080/math/mean/3/5
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
+        return math.mean(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+    }
+
+    // http://localhost:8080/math/squareRoot/81
+    @RequestMapping("/squareRoot/{numberOne}")
+    public double squareRoot(
+            @PathVariable("numberOne") String number
+    ) {
+
+        if (!NumberConverter.isNumeric(number))
+            throw new UnsupportedMathOperationException("SET A NUMERIC VALUE!");
+        return math.squareRoot(NumberConverter.convertToDouble(number));
+    }
+
 }
