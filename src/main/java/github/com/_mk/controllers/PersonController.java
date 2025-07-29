54114c8f19e8fe39b3acd1d2ee8094645e76d0fb
joao-marcos-de-jesus <933323mk@gmail.com>
2025-07-29 19:25:18 -0300

    CONTENT NEGOTIATION - implemento via QUERY params, via HEADER params, serializado via YML, testes da implementação

--- src/main/java/github/com/_mk/controllers/PersonController.java
+++ src/main/java/github/com/_mk/controllers/PersonController.java
@@ -19,35 +19,62 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonDTO create(@RequestBody PersonDTO person){
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
-    @PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonDTOV2 createV2(@RequestBody PersonDTOV2 person){
+    @PostMapping(value = "/v2", consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTOV2 createV2(@RequestBody PersonDTOV2 person) {
         return service.createV2(person);
     }
 
-    @PutMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonDTO update(@RequestBody PersonDTO person){
+    @PutMapping(consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
 
-//    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    @GetMapping(value = "/{id}", produces = {
+//    MediaType.APPLICATION_JSON_VALUE,
+//    MediaType.APPLICATION_XML_VALUE,
+//    MediaType.APPLICATION_YAML_VALUE})
 //    public PersonDTO findById(@PathVariable("id") Long id){
 //        var person = service.findById(id);
 //        person.setBirthDay(new Date());
@@ -55,8 +82,11 @@ public class PersonController {
 //        return person;
 //    }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<PersonDTO> findAll(){
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+    public List<PersonDTO> findAll() {
         return service.findAll();
     }
 }
